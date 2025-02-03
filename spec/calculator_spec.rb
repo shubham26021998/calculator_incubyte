eb3485c9a2d69e11960616d49c427cc56661de41
Shubham Goel <shubham.goel@anchanto.com>
2025-02-03 14:11:38 +0530

    rspec error NoMethodError: add

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require './calculator'
+
+describe do
+  context "Add the values given in string" do
+    it "returns 0 for an empty string" do
+      added_value = add("")
+      expect(added_value).to eq(0)
+    end
+  end
+end
