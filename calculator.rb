fd7a6a9678d69b7a439311ac579e52742df7e85f
Shubham Goel <shubham.goel@anchanto.com>
2025-02-03 16:11:08 +0530

    handled split using regex to split using both the delimeter

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 def add(str="")
   return 0 if str.nil?
-  nums = str.split(",").map{ |num|
+  nums = str.split(/,|\n/).map{ |num|
     raise ArgumentError unless num.match?(/^\d+$/)
     num.to_i
   }
