a1b83b37830551531ce972f3a66639d913b519f6
Shubham Goel <shubham.goel@anchanto.com>
2025-02-03 16:48:02 +0530

    handled custom delimeter

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,20 @@
 def add(str="")
   return 0 if str.nil?
-  raise ArgumentError if str.match?(/\n,|,\n|,,|\n\n/)
-  nums = str.split(/,|\n/).map{ |num|
+
+  if str.start_with?("//")
+    delimiter_part, numbers_part = str.split("\n", 2)
+    delimiter = delimiter_part[2..]
+  else
+    delimiter = ",|\n"
+    numbers_part = str
+  end
+
+  raise ArgumentError if numbers_part.match?(/\n,|,\n|,,|\n\n/)
+
+  nums = numbers_part.split(/#{delimiter}|\n/).map do |num|
     raise ArgumentError unless num.match?(/^\d+$/)
     num.to_i
-  }
+  end
+
   nums.sum
 end
