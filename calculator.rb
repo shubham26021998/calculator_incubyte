eae4bb256bb64585d220ab57a01ac7c4c20d14d9
Shubham Goel <shubham.goel@anchanto.com>
2025-02-03 14:46:12 +0530

    Refactored the logic

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,3 @@
 def add(str="")
-  ans = 0
-  if(str == "")
-    ans = 0
-  elsif (!str.include?(","))
-    ans = str.to_i
-  else
-    ans = str.split(",").map(&:to_i).sum
-  end
-  ans
+  ans = str.split(",").map(&:to_i).sum
 end
