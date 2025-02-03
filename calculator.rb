e158b554588a168315b1ce22759edefca6098589
Shubham Goel <shubham.goel@anchanto.com>
2025-02-03 17:16:51 +0530

    handled TC for negetive numbers

--- calculator.rb
+++ calculator.rb
@@ -9,12 +9,15 @@ def add(str="")
     numbers_part = str
   end
 
-  raise ArgumentError if numbers_part.match?(/\n,|,\n|,,|\n\n/)
+  raise ArgumentError, "Invalid format" if numbers_part.match?(/\n,|,\n|,,|\n\n/)
 
   nums = numbers_part.split(/#{delimiter}|\n/).map do |num|
-    raise ArgumentError unless num.match?(/^\d+$/)
+    raise ArgumentError, "Invalid input: #{num}" unless num.match?(/^-?\d+$/)
     num.to_i
   end
+
+  negatives = nums.select(&:negative?)
+  raise ArgumentError, "Negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
 
   nums.sum
 end
